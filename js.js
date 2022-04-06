c8c1b93d3fa5ead3f1faa4ee3315e3399f8415e0
Yonas <yonie3124@gmail.com>
2022-04-06 13:52:50 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+//Declare a function factorialize(n) that takes an integer n as input and returns the factorial of the provided integer.
+
+// first need to declare whe initial variable and its value to 1. 
+// then declare a loop that starts from 1 and less than or equal to the number need to calculate the factorial, then increment the initial value by one for every step
+// then multiply the first multi variable with every i
+// then console.log()
+
+
+let multi = 1;
+for(i = 1; i <= 5; i++){
+  multi = multi * i;
+
+}
+  console.log(multi);
+
